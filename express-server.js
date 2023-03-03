eb2ca28e6b8f08d6f0c15b82061decc0aabf02be
Daniel Soares <danielsoares01@outlook.com>
2023-03-03 12:01:10 -0800

    express update

--- express-server.js
+++ express-server.js
@@ -11,9 +11,22 @@ const urlDatabase = {
 //   res.send("Hello!");
 // });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
