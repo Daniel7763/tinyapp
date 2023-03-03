e7495d5bbf23c98fe9d4334f2d5c7390201c1605
Daniel Soares <danielsoares01@outlook.com>
2023-03-03 13:06:22 -0800

    express erver update

--- express-server.js
+++ express-server.js
@@ -21,6 +21,14 @@ const urlDatabase = {
 //   res.send("<html><body>Hello <b>World</b></body></html>\n");
 // });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
