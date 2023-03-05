bc266ee002262b97a043ca2f14eb2ff6f844e097
Daniel Soares <danielsoares01@outlook.com>
2023-03-05 11:15:20 -0800

    add function to redirect to long url

--- express-server.js
+++ express-server.js
@@ -4,7 +4,7 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
-const generateRandonString = function() {
+const generateRandomString = function() {
   let chars = 'abcdefghijklmnopqrstuvwxyz123456789';
   let charLength = chars.length;
   let result = '';
@@ -14,11 +14,18 @@ const generateRandonString = function() {
   return result;
 };
 
+const addUrlToDatabase = function(longUrl, shortUrl) {
+  urlDatabase[shortUrl] = longUrl;
+  console.log(urlDatabase);
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+// addUrlToDatabase(urlDatabase, "https://hoyt.com/", "xbjj76");
+
 
 // app.get("/", (req, res) => {
 //   res.send("Hello!");
@@ -34,22 +41,32 @@ const urlDatabase = {
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({ extended: true }));
 
+//takes input urls and adds a long $ short url to the database
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const longUrl = req.body.longURL;
+  const shortUrl = generateRandomString();
+  addUrlToDatabase(longUrl, shortUrl);
+
+  res.redirect(`/urls/${shortUrl}`);
+  
+  // res.send("Ok, we will replace this."); // Respond with 'Ok' (we will replace this)
+
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+app.get("/u/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  res.redirect(longURL);
 });
 
+//create new URL page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//?? not sure what this one is supposed to do
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
@@ -58,20 +75,24 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+//My URLs, Homepage
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//displays a=1
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
 });
  
+
 app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
 });
 
+//displays in terminal server listening on which port
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
