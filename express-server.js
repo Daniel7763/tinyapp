abc563cc4c0988655c11a6fea995f462c3013d30
Daniel Soares <danielsoares01@outlook.com>
2023-03-19 20:07:02 -0700

    more permission updates for urls:id, urls/delete, core program functionality

--- express-server.js
+++ express-server.js
@@ -21,9 +21,11 @@ const generateRandomString = function() {
 };
 
 //add url to database function
-const addUrlToDatabase = function(longUrl, shortUrl) {
-  urlDatabase[shortUrl] = longUrl;
-  // console.log(urlDatabase);
+const addUrlToDatabase = function(longUrl, shortUrl, userID) {
+  //--url update--
+  // urlDatabase[shortUrl] = longUrl;
+  urlDatabase[shortUrl] = {longUrl, userID};
+  console.log(urlDatabase);
 };
 
 //search for email in users object
@@ -36,20 +38,29 @@ const getUserByEmail = function(emailInput) {
   return null;
 };
 
+//filter database for a specific users url's
+const urlsForUser = (id, urlDatabase) => {
+  let userURLdata = {};
+  for (const url in urlDatabase) {
+    if (id === urlDatabase[url].userID) {
+      userURLdata[url] = urlDatabase[url];
+    }
+  }
+  return userURLdata;
+};
+
 //-------------DATABASES------------
 
 //url database object
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
-  9sm5xK: "http://www.google.com"
-  // b2xVn2: {
-  //   longURL: "http://www.lighthouselabs.ca",
-  //   userID: "aJ48lW",
-  // },
-  // 9sm5xK: {
-  //   longURL: "http://www.google.com",
-  //   userID: "aJ48lW",
-  // },
+  b2xVn2: {
+    longUrl: "http://www.lighthouselabs.ca",
+    userID: "aJ48lW",
+  },
+  '9sm5xK': {
+    longUrl: "http://www.google.com",
+    userID: "aJ48lW",
+  },
 };
 
 //users account object
@@ -119,60 +130,113 @@ app.post("/login", (req, res) => {
 
 //Logout
 app.post("/logout", (req, res) => {
+  
   res.clearCookie("user_id");
   res.redirect('/login');
 });
 
 //Delete button
 app.post("/urls/:id/delete", (req, res) =>{
-  const id = req.params.id;
-  delete urlDatabase[id];
-  res.redirect('/urls');
+  const userCookie = req.cookies["user_id"];
+  if (!userCookie) {
+    return res
+      .status(400)
+      .send('Need to be logged in to delete urls');
+  } else {
+    const userURLs = urlsForUser(userCookie, urlDatabase);
+    const urlToDelete = req.params.id;
+    if (userURLs[urlToDelete]) {
+      delete urlDatabase[urlToDelete];
+      res.redirect('/urls');
+    } else {
+      return res
+        .status(400)
+        .send('only the creator of a url can delete ');
+    }
+  }
 });
+//   const id = req.params.id;
+//   delete urlDatabase[id];
+//   res.redirect('/urls');
+// });
 
 //takes input urls and adds a long $ short url to the database
 app.post("/urls", (req, res) => {
-  // console.log(req.body); // Log the POST request body to the console
   const longUrl = req.body.longURL;
   const shortUrl = generateRandomString();
-  addUrlToDatabase(longUrl, shortUrl);
 
-  res.redirect(`/urls/${shortUrl}`);
+  if (!req.cookies['user_id']) {
+    return res.send('please login');
+  }
+
+  addUrlToDatabase(longUrl, shortUrl, req.cookies["user_id"]);
+
+  return res.redirect(`/urls/${shortUrl}`);
   // res.send("Ok, we will replace this."); // Respond with 'Ok' (we will replace this)
 });
 
-//Add url to url database
+//shorturl display/ undefined url
 app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
   const newUrl = req.body.newUrl;
   urlDatabase[id] = newUrl;
+  const userCookie = req.cookies.users_id;
+
+  //--show message if click edit on _index and shorturl not in users object--
+
+  //--bug start. found that causes a non login issue--
+  // if (urlDatabase[id].user_id !== req.cookies.users_id) {
+  //   return res
+  //     .status(400)
+  //     .send('Please <a href="/login">login</a> to view your short URLs');
+  // }
+
+  //--login request message if click edit on _index while logged out--
+  if (!userCookie) {
+    return res.redirect("urls_login");
+  }
+  //bug end.
+
+  //--if user in database--
+  if (!urlDatabase[req.params.id]) {
+    return res
+      .status(400)
+      .send("url not in database");
+  }
+  //--if shorturl isnt in your user object show warning--
+  if (urlDatabase[req.params.id].userID !== req.user_id) {
+    return res
+      .status(400)
+      .send('You lack permission to see this url.');
+  }
 
   res.redirect('/urls');
-//   console.log(req.body);
-// res.end(JSON.stringify(req.body));
 });
-
-//urls not logged in message
-app.post("/urls", (req, res) => {
-  if(!user)
-})
 
 //------------GET---------------
 
 //something to help add the username to index
 app.get("/urls", (req, res) => {
+  console.log(urlDatabase, "urlDatabase");
+
   const cookieLogin = req.cookies["user_id"];
   const templateVars = {
     user: users[cookieLogin],
-    urls: urlDatabase
+    urls: urlsForUser(req.cookies["user_id"], urlDatabase)
   };
+  // hide short urls /display message if not signed in
+  // if (!req.cookies["user_id"]) {
+  //   return res.send("please login or register to start making short Urls");
+
   res.render("urls_index", templateVars);
 });
 
 //redirects to short url website
 app.get("/u/:id", (req, res) => {
   const id = req.params.id;
-  const longURL = urlDatabase[id];
+  //--url update--
+  // const longURL = urlDatabase[id];
+  const longURL = urlDatabase[id].longUrl;
   res.redirect(longURL);
 });
 
@@ -227,9 +291,27 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = {
     user: users[cookieLogin],
     id: req.params.id,
-    longURL: urlDatabase[req.params.id]
   };
-  res.send("this url does not exist");
+  //--no user logged in/ redirect to login--
+  if (!cookieLogin) {
+    return res. redirect("/login");
+  }
+
+  //--url does not exist/ send error message
+  if (!urlDatabase[req.params.id]) {
+    console.log(req.params.id);
+    return res
+      .status(400)
+      .send("url does not exist");
+  }
+
+  //--if cookie does not match logged in user/ send to login--
+  if (urlDatabase[req.params.id].userID !== cookieLogin) {
+    console.log(`${urlDatabase[req.params.id].userID}, req. params`);
+    console.log(`${cookieLogin}, cookielogin`);
+    return res.send("access to this url is blocked");
+  }
+  return res.render("urls_show", templateVars);
 });
 
 
